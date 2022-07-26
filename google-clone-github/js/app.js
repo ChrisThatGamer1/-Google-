b57d388542d3a4175008375e20c38661348cbccc
ChrisThatGamer1 <50189496+ChrisThatGamer1@users.noreply.github.com>
2022-07-25 22:24:55 -0400

    Second Commit - Google Clone is done
    You can search using the search bar, each icon button works and the footer also was updated. Overall clone site is complete. Need to now move it over to a static URL.

--- google-clone-github/js/app.js
+++ google-clone-github/js/app.js
@@ -1,1 +1,5 @@
-console.log("Yo learn for your fucking self");
+const input = document.getElementById("search");
+    if(screen.width >= 820) {
+      document.querySelector("#footer-left").innerHTML =
+      '<li><a href="https://www.google.com/search/howsearchworks">How does the search work</a></li><li><a href="https://about.google/">About Google</a></li><li><a href="https://ads.google.com">Advertising</a></li><li><a href="https://smallbusiness.withgoogle.com/">Business</a></li>';
+    }
